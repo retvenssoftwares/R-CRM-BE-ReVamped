6c834d7c01f62b3728462c255a2c7b97090ba6c4
Deep142mah <deepali.zeeweesoft@gmail.com>
2023-12-05 14:41:07 +0530

    Add admin as seeder

--- model/User.js
+++ model/User.js
@@ -7,15 +7,15 @@ const userSchema = new Schema(
     name: { type: String },
     email: { type: String },
     phone_number: { type: Number },
-    gender : { type : String , enum : ['M', 'F'] },
+    gender : { type : String , enum : ['MALE', 'FEMALE','OTHER'] },
     DOB : { type: String },
     password: { type: String, required: true},
     role : { type : String , enum : ['ADMIN', 'AGENT', 'SUPER_ADMIN'] },
     status : { type : String , enum : ['ACTIVE', 'INACTIVE'] },
-    isVarified : { type: Boolean, default: false },
+    is_verified : { type: Boolean, default: false },
     otp : { type: Number },
     expires: {type : Date},
-    isEmailVarified : { type: Boolean, default: false}
+    is_email_Verified : { type: Boolean, default: false}
   },
   { versionKey: false, timestamp : true }
 );
