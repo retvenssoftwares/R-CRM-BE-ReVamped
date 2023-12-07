82ff48d035c1508d152d51559f289a3cd25fc2cb
Deep142mah <deepali.zeeweesoft@gmail.com>
2023-12-07 10:47:57 +0530

    Disposition graph

--- controller/Agent.js
+++ controller/Agent.js
@@ -491,6 +491,12 @@ class AgentModel {
         });
       }
 
+      if (req.authData.role === "ADMIN") {
+        condition.unshift({ $match: { admin_id: req.authData._id } });
+      } else if (req.authData.role === "AGENT") {
+        condition.unshift({ $match: { agent_id: req.authData._id } });
+      }
+
       let findCalls = await callDetails.aggregate(condition);
 
       let result = findCalls.reduce((obj, itm) => {
