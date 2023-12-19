8c8a85e9821bb521afdd0c35226986a753eddb33
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-12-19 13:31:51 +0530

    Report api

--- routes/Reports.routes.js
+++ routes/Reports.routes.js
@@ -7,5 +7,6 @@ router.get('/getCallVolume',verifyJwt,report.getCallVolumeReport)
 router.get('/getAvgCallDuration',verifyJwt,report.getCallDurationReport)
 router.get('/getCallOutCome',verifyJwt,report.getCallOutComeReport)
 router.get('/getCallResolution',verifyJwt,report.getFirstCallResolutionReport)
+router.get('/getAgentPerformance',verifyJwt,report.getAgentPerformance)
 
 export default router
